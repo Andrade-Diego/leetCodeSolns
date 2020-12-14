c43c7fbd6bb195a7a5b0cda2dab582432d952f60
Diego Andrade <daan222@uky.edu>
2020-12-14 17:02:50 -0500

    Update to twoSum.cpp
    Includes a main function now with a test case.

--- twoSum.cpp
+++ twoSum.cpp
@@ -1,24 +1,41 @@
 #include <map>
 #include <vector>
+#include <iostream>
 using namespace std;
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> myMap; //key is a value needed to be found, value is index of it's mate to meet target
-        vector<int> soln; //will contain index of solution
-        for(int i = 0; i < nums.size(); i++){
-            if(myMap.count(nums[i]) == 1){ // if a map key exists with the current i's value
-                cout << myMap[nums[i]]<< ' ' << i << endl;
+vector<int> twoSum(vector<int>& nums, int target) {
+    //key is a value needed to be found, value is index of it's mate to meet target
+    map<int, int> myMap;
 
-                //put indices in soln
-                soln.push_back(i); 
-                soln.push_back(myMap[nums[i]]);
-                break;
-            }
-            myMap.insert(pair<int, int>(target - nums[i], i));// give map the number it needs to get to target and current index
+    //will contain index of solution
+    vector<int> soln;
 
+    //loop through nums 
+    for(int i = 0; i < nums.size(); i++){
+
+        // if a map key exists with the current i's value
+        if(myMap.count(nums[i]) == 1){
+
+            //put indices in soln
+            soln.push_back(i); 
+            soln.push_back(myMap[nums[i]]);
+
+            //end early
+            break;
         }
-        return soln;
+
+        // give map the number it needs to get to target and current index
+        myMap.insert(pair<int, int>(target - nums[i], i));
+
     }
-};
+    return soln;
+}
+
+int main(){
+    // This is a testcase
+    cout <<"(TEST)\n\tList = {2, 7, 11, 15}\n\tTarget = 9" << endl;
+    vector<int> nums{2,7,11,15};
+    int target = 9;
+    vector<int> result = twoSum(nums, target);
+    cout << "\tResult = [" << result[0] << ", " << result[1] << "]" << endl;
+}
